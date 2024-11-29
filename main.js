30129baf1ee8b517384ecdb497d382796e6dd8b5
SuportCoop <s18023647@gmail.com>
2024-11-29 09:36:42 -0300

    Tabela Fipe

---
+++ main.js
@@ -0,0 +1,21 @@
+async function BuscarFipe() {
+
+    const idBrand = document.getElementById('brand').value
+    //testnado
+    const url =`https://parallelum.com.br/fipe/api/v1/motos/marcas/${idBrand}/modelos/9265/anos/2024-1`;
+    // const url = `https://parallelum.com.br/fipe/api/v1/motos/marcas/`+ idBrand+`/modelos/9265/anos/2024-1`
+
+    await fetch(url).then((response) => {
+        // Transforma JSON em OBJ
+        return response.json();
+
+    }).then((response) => {
+
+        document.getElementById('brand').value= response.Marca
+        document.getElementById('model').value= response.Modelo
+        document.getElementById('value').value = response.Valor
+        document.getElementById('fuel').value = response.Combustivel
+        document.getElementById('year_factory').value = response.AnoModelo
+
+    })
+}
